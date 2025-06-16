6fe976e43d2eed26c7b36ce4ac6838933c9d38a5
T0dS1K <124768774+T0dS1K@users.noreply.github.com>
2025-06-17 01:02:43 +0300

    Серверное приложение

---
+++ VKRServer/VKRServer/Models/Data.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Immutable;
+using System.Numerics;
+
+namespace VKRServer.Models
+{
+    public class Data
+    {
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+    }
+
+    public class PDH
+    {
+        public string? A { get; set; }
+        public string? p { get; set; }
+    }
+
+    public class SecretData
+    {
+        public int ID { get; set; }
+        public BigInteger Key { get; set; }
+    }
+
+    public class Refresh
+    {
+        public string? Token { get; set; }
+    }
+
+    public class TempDataCon
+    {
+        public ImmutableDictionary<int, string> Data { get; set; } = ImmutableDictionary<int, string>.Empty;
+    }
+}
