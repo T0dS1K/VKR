ddd8110a1c9757ad085997b7d8fbc1da41da72c2
T0dS1K <124768774+T0dS1K@users.noreply.github.com>
2025-06-19 18:15:22 +0300

    Update HomeFragmentModer.java

--- VKRAndroid/app/src/main/java/com/example/vkr/ui/home/moder/HomeFragmentModer.java
+++ VKRAndroid/app/src/main/java/com/example/vkr/ui/home/moder/HomeFragmentModer.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.graphics.Color;
 import android.view.ViewGroup;
-import android.graphics.Bitmap;
 import android.view.LayoutInflater;
-import androidx.lifecycle.Observer;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -16,44 +14,35 @@ public class HomeFragmentModer extends Fragment
 {
     private FragmentHomeModerBinding binding;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        HomeViewModelModer HomeViewModel =
-                new ViewModelProvider(this).get(HomeViewModelModer.class);
+        HomeViewModelModer homeViewModel = new ViewModelProvider(this).get(HomeViewModelModer.class);
 
         binding = FragmentHomeModerBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        HomeViewModel.GetQR().observe(getViewLifecycleOwner(), new Observer<Bitmap>()
+        homeViewModel.GetQR().observe(getViewLifecycleOwner(), QR ->
         {
-            @Override
-            public void onChanged(Bitmap QR)
+            if (QR != null)
             {
-                if (QR != null)
-                {
-                    binding.QRImageView.setImageBitmap(QR);
-                }
+                binding.QRImageView.setImageBitmap(QR);
             }
         });
 
-        HomeViewModel.GetTimer().observe(getViewLifecycleOwner(), new Observer<Long>()
+        homeViewModel.GetTimer().observe(getViewLifecycleOwner(), Timer ->
         {
-            @Override
-            public void onChanged(Long Timer)
+            if (Timer != null)
             {
-                if (Timer != null)
-                {
-                    float maxTimer = 10;
-                    float minTimer = 0;
+                float maxTimer = 10;
+                float minTimer = 0;
 
-                    float progress = (Timer - minTimer) / (maxTimer - minTimer);
-                    progress = Math.max(0f, Math.min(1f, progress));
+                float progress = (Timer - minTimer) / (maxTimer - minTimer);
+                progress = Math.max(0f, Math.min(1f, progress));
+                float[] hsv = {0f, 1f, 1 - progress};
 
-                    float[] hsv = {0f, 1f, 1 - progress};
-
-                    binding.Timer.setTextColor(Color.HSVToColor(hsv));
-                    binding.Timer.setText(String.valueOf(Timer));
-                }
+                binding.Timer.setTextColor(Color.HSVToColor(hsv));
+                binding.Timer.setText(String.valueOf(Timer));
             }
         });
 
